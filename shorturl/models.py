adf751fbb762702ef5a4fd02a82566e6f083fec6
KHANDELWAL <mohitkh7@gmail.com>
2017-12-01 12:28:39 +0530

    Plural Name of urls

--- shorturl/models.py
+++ shorturl/models.py
@@ -6,6 +6,9 @@ class urls(models.Model):
     httpurl = models.URLField(max_length=200)
     pub_date = models.DateTimeField(auto_now=True)
     count = models.IntegerField(default=0)
- 
-def __str__(self):
-    return self.httpurl
+
+    def __str__(self):
+        return self.httpurl
+
+    class Meta:
+    	verbose_name_plural = 'urls'
