00d31a3837944abae48f37b03e4b3a28e7c5a1b0
Mohit Khandelwal <mohitkh7@gmail.com>
2017-12-15 22:07:34 +0530

    #Issue 31 Test cases added (#43)
    * resolved contribute button hidden on small screen bug
    
    * Revert "Issue #25 QR Code Generator"
    
    * test cases for url shortening added
    
    * Revert "test cases for url shortening added"
    
    This reverts commit 375b3d3758da03e478c2d733c4ec54ec6ada1c40.
    
    * Revert "missing files added"
    
    This reverts commit 27d0983906e4b4b6af20e61ff621b7951ac67255.
    
    * Revert "Revert "missing files added""
    
    This reverts commit eacda47843b9e3aba650d8f214e68f28a0d5a7d1.
    
    * Revert "Revert "Issue #25 QR Code Generator""
    
    This reverts commit 39ffdc44a92d4fb905b0e763968a8030c6d805f8.
    
    * Revert "Revert "test cases for url shortening added""
    
    This reverts commit c60ae5c39931cf22fce900bbf0e33c05890a2c0c.

--- shorturl/tests.py
+++ shorturl/tests.py
@@ -1,3 +1,60 @@
+import json
+
 from django.test import TestCase
+from django.urls import reverse
+
+from .views import is_already_short
 
 # Create your tests here.
+class IndexViewTests(TestCase):
+	def test_default_index(self):
+		'''
+			test whether default home page is rendered or not
+		'''
+		response = self.client.get(reverse('shorturl:home'))
+		self.assertEquals(response.status_code,200)
+
+class MakeShortViewTests(TestCase):
+	def test_shortening_normal_url(self):
+		'''
+			test whether normal URL is shortened or not.
+		'''
+		url = "https://www.github.com"
+		response = self.client.post(reverse('shorturl:shortenurl'),data={'url':url})
+		# Decode UTF-8 bytes to Unicode, and convert single quotes to double quotes to make it valid JSON
+		str_content = response.content.decode('utf8').replace("'", '"')
+		json_content = json.loads(str_content)
+		self.assertNotEquals(json_content['url'],"")
+		self.assertEquals(response.status_code,200)
+
+	def test_shortening_empty_url(self):
+		'''
+			test whether empty URL generates error or not
+		'''
+		url = ""
+		response = self.client.post(reverse('shorturl:shortenurl'),data={'url':url})
+		# Decode UTF-8 bytes to Unicode, and convert single quotes to double quotes to make it valid JSON
+		str_content = response.content.decode('utf8').replace("'", '"')
+		json_content = json.loads(str_content)
+		self.assertEquals(json_content['valid_url'],False)
+
+	def test_shortening_shorteded_url(self):
+		'''
+			test whether already shorteded URL generates error or not
+		'''
+		url = "bit.ly/xyz"
+		response = self.client.post(reverse('shorturl:shortenurl'),data={'url':url})
+		# Decode UTF-8 bytes to Unicode, and convert single quotes to double quotes to make it valid JSON
+		str_content = response.content.decode('utf8').replace("'", '"')
+		json_content = json.loads(str_content)
+		self.assertEquals(json_content.get('already_short'),True)
+
+'''
+To test all the support function in views
+'''
+class ViewFunctionsTests(TestCase):
+	def test_is_already_short_function(self):
+		url = "https://www.github.com"
+		self.assertEquals(is_already_short(url),False)
+		url = "bit.ly/xyz"
+		self.assertEquals(is_already_short(url),True)
